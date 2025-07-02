9e7cefec040ed2949576fb2a2b937cab10f86b28
Anton-Garno <garnogivo@gmail.com>
2025-07-02 19:45:24 +0200

    Add project files.

---
+++ homework-10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Homework10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eenter your name :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your Family name(Surname) ");
+            string familyName = Console.ReadLine();
+            char[] familyNameArray = familyName.ToCharArray();
+            char [] nameArray = name.ToCharArray();
+            if (familyNameArray[0] == nameArray[0])
+            {
+                Console.WriteLine("Your name and family name starts with the same letter.");
+            }
+            else
+            {
+                Console.WriteLine("Your name and family name do not start with the same letter.");
+            }
+
+        }
+    }
+}
+
+    
+    
+
---
+++ homwwork-10.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Homework10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some words (like this, like some words) :");
+            string input = Console.ReadLine()??" ";
+
+            StringBuilder myStringInput = new StringBuilder(input);
+
+            myStringInput.Replace(" ", "");
+            Console.WriteLine("Your input without spaces is: " + myStringInput.ToString());
+
+
+
+
+        }
+    }
+}
+
+
